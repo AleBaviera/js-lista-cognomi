77b6ed3304baa357f581bbc735502a2e6106a666
alebaviera <baviera.alessandra@gmailcom>
2019-07-03 13:52:54 +0200

    struttura

---
+++ script.js
@@ -0,0 +1,22 @@
+// Chiedi all’utente il cognome, inseriscilo in un array con altri cognomi e stampa la lista ordinata alfabeticamente.
+// Scrivi a che posizione della lista il nuovo utente si trova
+
+
+
+// dichiarazione var e array
+
+
+
+// inserisci cognome
+
+
+
+// push nell'array
+
+
+
+// stampa la lista ordinata
+
+
+
+// restituisci posizione in elenco
