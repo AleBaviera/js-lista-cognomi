8f37a1593453ef43dbae02af68398c19c2ceba9f
alebaviera <baviera.alessandra@gmailcom>
2019-07-03 14:48:19 +0200

    bozza js

--- script.js
+++ script.js
@@ -2,13 +2,21 @@
 // Scrivi a che posizione della lista il nuovo utente si trova
 
 
-
+console.log('ciao');
 // dichiarazione var e array
-
-
+var tuocognome, elenco, listacognomi;
+listacognomi = document.getElementById('mio-id');
+elenco = [
+  'Bianchi',
+  'Rossi',
+  'Carli',
+  'Giuli',
+  'Viola',
+  'Blu'
+];
 
 // inserisci cognome
-
+tuocognome = prompt('Inserisci il tuo cognome');
 
 
 // push nell'array
@@ -17,6 +25,6 @@
 
 // stampa la lista ordinata
 
-
+listacognomi.innerHTML = elenco.sort()
 
 // restituisci posizione in elenco
